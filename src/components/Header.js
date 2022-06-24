8b596e9b4376526771e90e6462eb2dc1b45752e1
tushars882 <tushars882@gmail.com>
2022-06-24 17:39:32 +0530

    adding all files

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="banner">
+      <div className="banner-content">
+        <div className="container">
+          <div className="banner_text">
+            <h3 style={{ fontSize: "65px" }}>Restro "&" Retro</h3>
+            <h1 style={{ fontSize: "85px" }}>Drinks And Food</h1>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
+              impedit fugit repellendus quam ab beatae{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
