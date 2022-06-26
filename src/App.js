eb957253a5303157a5687e113924c16e7f00b3c8
tushars882 <tushars882@gmail.com>
2022-06-26 13:50:57 +0530

    adding bg attachment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { Shop } from './components/Shop';
 import { Header } from './components/Header';
+import { About } from './components/About';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <About/>
+      <Shop/>
     </div>
   );
 }
---
+++ src/components/Shop.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Shop = () => {
+  return (
+    <div className='shop'>
+       <div className="container">
+        <div className="shop_text">
+            <div>
+                <h1>Enjoy Free Delivery As Well</h1>
+                <div className="shop_btn">
+                    <a href="" className='btn btn-smart'>
+                    Shop Now    
+                    </a>
+                </div>
+            </div>
+        </div>
+       </div>
+    </div>
+  )
+}
